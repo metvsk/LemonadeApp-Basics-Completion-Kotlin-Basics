53ca970253ff7783da999388df505997879d77c9
metvsk <68542818+metvsk@users.noreply.github.com>
2021-10-18 12:39:53 +0530

    finished

--- app/src/main/java/com/chillandcode/lemonadeappkotlin/MainActivity.kt
+++ app/src/main/java/com/chillandcode/lemonadeappkotlin/MainActivity.kt
@@ -9,14 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     private var directionsTextView: TextView? = null
     private var imageView: ImageView? = null
-
-    //preparing list of scenes in android app with corresponding directions
-    private var scene = listOf<SceneData>(
-        SceneData("Tap to pluck Lemon", R.drawable.lemon_tree),
-        SceneData("Tap squeeze lemon", R.drawable.lemon_squeeze),
-        SceneData("Tap to drink lemon", R.drawable.lemon_drink),
-        SceneData("Tap to prepare lemonade again", R.drawable.lemonade_restart),
-    )
+    private var scene = listOf<SceneData>()
 
     //setting initial scene to 0
     private var currentScene: Int = 0
@@ -29,6 +22,20 @@ class MainActivity : AppCompatActivity() {
         directionsTextView = findViewById(R.id.textView)
         imageView = findViewById(R.id.imageView)
 
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        //preparing list of scenes in android app with corresponding directions
+        this.scene = listOf<SceneData>(
+            SceneData("Tap to pluck Lemon", R.drawable.lemon_tree),
+            SceneData("Tap squeeze lemon", R.drawable.lemon_squeeze),
+            //understanding string literals
+            SceneData(resources.getString(R.string.ingredient_string,3,"Sugar Cubes"), R.drawable.lemon_drink),
+            SceneData("Tap to drink lemon", R.drawable.lemon_drink),
+            SceneData("Tap to prepare lemonade again", R.drawable.lemonade_restart),
+        )
+
         //implementing touch listener to call next scene by updating the UI as the listener is invoked
         findViewById<ImageView>(R.id.imageView).setOnClickListener {
             currentScene++;
@@ -39,8 +46,6 @@ class MainActivity : AppCompatActivity() {
         //setting the initial scene
         updateScene(currentScene)
     }
-
-
 
 
     //called to update the scene according to the active scene int
